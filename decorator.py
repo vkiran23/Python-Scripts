cc145d63c44edc909ac7f4863cec9680b1956ee4
vkiran23 <kumar.kiranm24@gmail.com>
2023-11-06 18:25:30 +0530

    done decorators

---
+++ decorator.py
@@ -0,0 +1,72 @@
+# Decorators are Python functions that allow you to
+# wrap another function as an input and modify its 
+# behavior without altering the wrapped function’s code.
+# They are used to extend the behavior of a particular object, 
+# such as a class, method, or function. This approach promotes 
+# reusability, modularity, and separation of concerns in your Python programs.
+
+
+# Use cases for Decorators
+# Decorators are widely used for a variety of purposes, such as:
+
+# Timing function execution
+# Caching results (memoization)
+# Authorization and authentication
+# Logging and error handling
+# Input validation
+
+
+
+# built-in decorators
+# 1.property
+# 2.staticmethod
+# 2.classmethod
+# 4.abstract method
+
+
+# def outerfun(func):
+#     def wrapper():
+#         func()
+#         func()
+#     return wrapper
+
+# @outerfun
+# def twice():
+#    print( 'Hello World!')
+
+# twice()
+
+
+
+
+
+
+# import time
+
+# def timer(func):
+#     def wrapper(*args,**kwargs):
+#         start_time=time.time()
+#         result=func(*args,**kwargs)
+#         end_time=time.time()
+#         execution_time=start_time-end_time
+#         print(f'Execution time: {execution_time:.5f} seconds')
+#         return result
+#     return wrapper
+
+# @timer
+# def factorial(n):
+#     fact=1
+#     for i in range(1,n+1):
+#         fact*=i
+#         pass
+
+# print(factorial(3000))
+
+# @timer
+# def simple_function(n):
+#     for _ in range(n):
+#         pass
+
+# print(simple_function(10000))
+
+
